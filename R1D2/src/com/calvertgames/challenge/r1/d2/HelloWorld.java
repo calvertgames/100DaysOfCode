b25b608dfac8cc18a34e44debfa15fde9d1a2a8a
calvertgames <jonathancalvert@gmail.com>
2021-02-14 11:21:55 -0500

    R1D2 HelloWorld

---
+++ R1D2/src/com/calvertgames/challenge/r1/d2/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.calvertgames.challenge.r1.d2;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!  R1D2 Test");
+
+	}
+
+}
