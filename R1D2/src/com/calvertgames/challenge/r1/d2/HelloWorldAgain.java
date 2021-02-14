61e47aaf114fefd7373a7ff48c4063ea27b3c1a7
calvertgames <jonathancalvert@gmail.com>
2021-02-14 11:40:25 -0500

    R1D2 a second class ...

---
+++ R1D2/src/com/calvertgames/challenge/r1/d2/HelloWorldAgain.java
@@ -0,0 +1,10 @@
+package com.calvertgames.challenge.r1.d2;
+
+public class HelloWorldAgain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World Again!  R1D2 Test");
+	}
+
+}
